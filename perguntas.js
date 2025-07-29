455899cbc447ca5ed29a00bc9814030ba95b800e
Marymj7 <162161972+Marymj7@users.noreply.github.com>
2025-07-29 09:26:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ciencia',
+    'Quem foi a primeira pessoa a viajar no espaço?',
+    'Yuri Gagarin.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest. '
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
